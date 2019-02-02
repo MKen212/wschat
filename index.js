10c73f966955d3bf65fd765e6eb79181e8d554a7
Mark Kensington <46496040+MSK212@users.noreply.github.com>
2019-02-02 12:44:52 +0100

    Updated Console with Timestamp

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const wss = new WebSocket.Server({ port: 8080 });
 console.log("WebSocket Server running on port 8080");
 
 wss.on('connection', function connection(ws) {
-    console.log("Connected");
+    console.log("Connected: " + new Date());
     ws.on('message', function incoming(message) {
-        console.log('Incomming message');
+        console.log("Incomming message: " + new Date());
         wss.clients.forEach(function each(client) {
             client.send(message);
         });
